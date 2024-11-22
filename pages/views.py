a84d86888b11f6c84368bf7c28be2efb7d397f63
Nikki <bogdnico@gmail.com>
2024-11-21 23:24:39 -0600

    first

---
+++ pages/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+    template_name = "pages/about.html"
+
+# Create your views here.
